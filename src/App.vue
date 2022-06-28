444a6c4e0a37b0dee985a45c20032cd18a41bd52
Stars_Mo <944755143@qq.com>
2022-06-28 18:15:58 +0800

    chore: 引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <van-button type="primary">Vant-主要按钮</van-button>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import App from './App'
 import router from './router'
 import './index.css'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

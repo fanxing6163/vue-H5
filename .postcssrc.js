dbb7752604f08b099b016d942878d33330abbe4c
Stars_Mo <944755143@qq.com>
2022-06-29 09:38:27 +0800

    feat: px2viewport

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,6 +5,24 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {},
+    // https://github.com/evrone/postcss-px-to-viewport/blob/master/README.md
+    "postcss-px-to-viewport": {
+      unitToConvert: 'px',
+      viewportWidth: 375,
+      unitPrecision: 5,
+      propList: ['*'],
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      selectorBlackList: [],
+      minPixelValue: 1,
+      mediaQuery: false,
+      replace: true,
+      exclude: /node_modules/i,
+      include: undefined,
+      // landscape: false,
+      // landscapeUnit: 'vw',
+      // landscapeWidth: 568
+    }
   }
 }
